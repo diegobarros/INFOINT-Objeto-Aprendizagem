2f9b7d49083aa23d32768d87d45c057fcab03e7e
Diego Augusto de Faria Barros <diego@diego-IPMH61R2.(none)>
2012-10-17 14:09:46 -0300

    Criacao do projeto e instalacao activeadmin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+INFOINTObjetoAprendizagem::Application.config.secret_token = '73cd4e8f945ec72d05f91b2dabe33173d80e7ae0f02cf70671a674528823637a007868ad0974a42dd9608b68432a3638c192187cd3755440b50b03221c04a456'
